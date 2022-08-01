6b54bd85414d9f85a36647a611253216e9462147
Shivanshu5998s <96103252+Shivanshu5998s@users.noreply.github.com>
2022-08-01 12:16:57 +0530

    nw

---
+++ C1-27June2022/index1.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+// const port = 3000;
+
+app.post("/product/create", (req, res) => {
+    res.send(req.body);
+});
+
+app.listen(7000, () => {
+    const obj = { "websiteName": "masaischool.com" }
+    var obje = DNS.parse(stringify(obj));
+
+    console.log(obje);
+})
+
+var dns = require('dns');
+
+var w3 = dns.lookup('w3schools.com', function (err, addresses, family) {
+    console.log(addresses);
+});
---
+++ C1-27June2022/index2.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const products = require("./products.json");
+
+const app = express()
+app.use(express.json());
+
+app.get('/products', (req, res) => {
+    res.send({ products });
+})
+
+app.post('/products/create', (req, res) => {
+    const newProduct = [...products, req.body];
+    res.send(newProduct);
+})
+
+app.get('/products/:productid', (req, res) => {
+    const newProducts = products.filter((product) => product.id == req.params.id);
+    console.log(newProducts)
+    res.send(newProducts);
+})
+
+app.delete('/products/:productid', (req, res) => {
+    const newProducts = products.filter((product) => product.id == req.params.id);
+    console.log(newProducts)
+    res.send(newProducts);
+})
+
+app.listen(1234, function () {
+    console.log('listening on port 1234');
+})
